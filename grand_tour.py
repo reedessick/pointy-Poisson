3ee63a3842dd46b741c49c1d2c223f0272899385
Reed Essick <reed@rbe4.org>
2015-08-23 23:01:26 -0400

    began working on grand_tour.py; less convinced that this is worthwhile...

--- grand_tour.py
+++ grand_tour.py
@@ -3,10 +3,6 @@
 usage = """grand_tour.py [--options] gps gps gps..."""
 description="""a script that generates a pointed follow-up of possible auxiliary couplings, sweeping through a variety of windows and snr thresholds"""
 
-
-raise StandardError("MODIFY ME")
-
-
 import numpy as np
 
 from laldetchar.idq import idq
@@ -14,10 +10,125 @@ from laldetchar.idq import event
 
 import greedyCI as gci
 
+import matplotlib
+matplotlib.use("Agg")
+from matplotlib import pyplot as plt
+
 from optparse import OptionParser
 
 from ConfigParser import SafeConfigParser
 
+#=================================================
+
+def grand_tour(gps, trgs, trgtype, windows, conf=0.68, plot=False):
+    """
+    a function that compute the grand tour statistics 
+    """
+    if trgtype == "kw":
+        col = event.col_kw
+        snrkey = 'signif'
+    elif trgtype == "Omicron":
+        col = event.col_snglBurst
+        snrkey = 'snr'
+    else:
+        raise ValueError("do not understand trgtype=%s"%(trgtype))
+
+    figax = []
+    for win in windows:
+        print "window=%.6f"%win
+
+        ### downelect to only this window
+        trgs = [ trg for trg in trgs if (trg[col['tcent']] >= gps-win) and (trg[col['tcent']] <= gps+win) ]
+
+        Pvalue = np.infty
+        Snr = None
+        Dt = None
+        Frq = None
+        for snrThr in sorted(list(set([ trg[col[snrkey]] for trg in trgs ]))): ### iterate over all snrs present
+            print "snrThr=%.6f"%snrThr
+
+            ctrgs =  [ trg for trg in trgs if trg[snrkey] >= snrThr ]
+            n = len( ctrgs ) ### number of triggers
+            r = 0.5*n/opts.window ### point estimate of the rate
+
+            if n:
+                dt = np.array([ctrgs[col['tcent']] for trg in ctrgs]) - gps
+                arg = np.argmin(np.abs(dt))
+
+                snr = ctrgs[arg][col[snrkey]]
+                frq = ctrgs[arg][col['fcent']]
+                min_dt = dt[arg]
+                absmin_dt = abs(min_dt)
+            else:
+                min_dt = win
+                snr = None
+                frq = None
+
+            if r > 0:
+                pvalue = 1 - np.exp(-r*2*absmin_dt) ### cumulative probability of observing min_dt <= observed min_dt | estimated rate, the factor of 2 comes from looking on either side of the specified gps time
+            else:
+                pvalue = 1 ### limit is not great here...need to add CI
+
+            print "\n\tchannel=%s\n\t-> Ntrg=%d\n\t-> rate=%.9e Hz\n\t-> min_dt=%.9e sec\n\t-> pvalue=%.9e"%(chan, n, r, min_dt, pvalue)
+
+            r_l, r_h = np.array( gci.poisson_bs(conf, n) ) * 0.5 / win
+            pvalue_l = 1 - np.exp(-r_l*2*absmin_dt)
+            pvalue_h = 1 - np.exp(-r_h*2*absmin_dt)
+
+            print "\t-> %.5e confidence:\n\t\tlow rate =%.9e Hz\n\t\thigh rate=%.9e Hz\n\t\tlow pvalue =%.9e\n\t\thigh pvalue=%.9e"%(conf, r_l, r_h, pvalue_l, pvalue_h)
+
+            if pvalue_h < Pvalue:
+                pvalue = pvalue_h
+                Snr = snr
+                Dt = dt
+                Frq = frq
+
+        ### separate plot for each window
+        ### xaxis : time (relative to gps)
+        ### yaxis SNR
+        ### color = pvalue (upper limit?)
+        if plot:                 
+            fig = plt.figure()
+            ax = plt.subplot(1,1,1)
+            figax.append( (fig, ax) )
+
+            ### plot all triggers
+#            dts = np.array([ trg[col['tcent'] for trg in trgs ]) - gps
+#            snrs = [ trg[col[snrkey]] for trg in trgs ]
+#            frqs = [ trg[col['fcent']] for trg in trgs ]
+
+            for trg in trgs:
+                dt  = trg[col['tcent']] - gps
+                snr = trg[col[snrkey]]
+                frq = trg[col['fcent']]
+
+                color = snr_map( snr )
+                ax.plot( dt, frq, markerfacecolor=color, markeredgecolor='none', marker='o', linestyle='none', alpha=0.50, markersize=2 )
+
+            ax.set_xlabel( 'time relative to %.6f [sec]'%gps )
+            ax.set_ylabel( 'frequency [Hz]' )
+
+            if Dt:
+                color = snr_map( Snr )
+                ax.plot( Dt, Frq, markerfacecolor='none', markeredgecolor=color, marker='o', linestyle='none', alpha=1.00, markersize=5 )
+                ax.text( Dt, Frq, '%.3e'%Pvalue, ha='left', va='center' )
+
+            ax.set_xlim(xmin=-win, xmax=win)
+            ymax = 1
+            maxfrq = max( [ trg[col['fcent']] for trg in trgs ] )
+            while maxfrq > ymax:
+                ymax *= 2
+            ax.set_ylim(ymin=0, ymax=ymax)
+
+    return figax
+ 
+
+def snr_map( snr ):
+    """
+    map snr into color
+    """
+    return 'k' 
+ 
 #=================================================
 
 parser = OptionParser(usage="", description=description)
@@ -28,7 +139,6 @@ parser.add_option("", "--Omicronverbose", dest="overbose", default=False, action
 parser.add_option("", "--OfflineOmicronverbose", dest="ooverbose", default=False, action="store_true", help="make the retrieve_OfflineOmicronTrigs() call verbose")
 
 parser.add_option("-c", "--config", default="config.ini", type="string")
-parser.add_option("-w", "--window", default=10, type="float")
 
 parser.add_option("-o", "--output-dir", default="./", type="string")
 
@@ -49,6 +159,9 @@ config.read(opts.config)
 
 conf = config.getfloat('general', 'conf')
 ifo = config.get('general', 'ifo')
+windows = sorted([float(l) for l in config.get('general','windows').split()], reverse=True)
+
+maxwindow = max(windows)
 
 #===========
 
@@ -92,8 +205,8 @@ for gps in args:
     if kwchannels:
         ### go find triggers
         if opts.verbose:
-            print "\tdiscoverying KW triggers within [%.9f, %.9f]"%(gps-opts.window, gps+opts.window)
-        kwtrgdict = idq.retrieve_kwtrigs(kwgdsdir, kwbasename, int(np.floor(gps-opts.window)), 2*opts.window+1, kwstride, verbose=opts.kwverbose)
+            print "\tdiscoverying KW triggers within [%.9f, %.9f]"%(gps-maxwindow, gps+maxwindow)
+        kwtrgdict = idq.retrieve_kwtrigs(kwgdsdir, kwbasename, int(np.floor(gps-maxwindow)), 2*maxwindow+1, kwstride, verbose=opts.kwverbose)
     
         ### keep only the relevant channels
         if opts.verbose:
@@ -103,7 +216,7 @@ for gps in args:
         kwtrgdict.keep_channels(kwchannels)
 
         ### trim the edges
-        kwtrgdict.include([[gps-opts.window, gps+opts.window]], tcent=event.col_kw['tcent'])
+        kwtrgdict.include([[gps-maxwindow, gps+maxwindow]], tcent=event.col_kw['tcent'])
    
         ### ensure we have entries for all requested channels and downselect as needed
         if opts.verbose:
@@ -140,11 +253,11 @@ for gps in args:
     #=============================================
     if ochannels:
         if opts.verbose:
-            print "\tdiscovering Omicron triggers within [%.9f, %.9f]"%(gps-opts.window, gps+opts.window)
-        otrgdict = idq.retrieve_OmicronTrigs(ogdsdir, ifo, int(np.floor(gps-opts.window)), 2*opts.window+1, ostride, ochannels, verbose=opts.overbose)
+            print "\tdiscovering Omicron triggers within [%.9f, %.9f]"%(gps-maxwindow, gps+maxwindow)
+        otrgdict = idq.retrieve_OmicronTrigs(ogdsdir, ifo, int(np.floor(gps-maxwindow)), 2*maxwindow+1, ostride, ochannels, verbose=opts.overbose)
 
         ### trim edges
-        otrgdict.include([[gps-opts.window, gps+opts.window]], tcent=event.col_snglBurst['tcent'])
+        otrgdict.include([[gps-maxwindow, gps+maxwindow]], tcent=event.col_snglBurst['tcent'])
 
         ### downselect as needed
         if opts.verbose:
@@ -181,11 +294,11 @@ for gps in args:
     #=============================================
     if oochannels:
         if opts.verbose:
-            print "\tdiscovering OfflineOmicron triggers within [%.9f, %.9f]"%(gps-opts.window, gps+opts.window)
+            print "\tdiscovering OfflineOmicron triggers within [%.9f, %.9f]"%(gps-maxwindow, gps+maxwindow)
         ootrgdict = idq.retrieve_OfflineOmicronTrigs(oogdsdir, ifo, int(np.floor(gps-opts.window)), 2*opts.window+1, channels=oochannels, verbose=opts.ooverbose)
 
         ### trim edges
-        ootrgdict.include([[gps-opts.window, gps+opts.window]], tcent=event.col_snglBurst['tcent'])
+        ootrgdict.include([[gps-maxwindow, gps+maxwindow]], tcent=event.col_snglBurst['tcent'])
 
         ### downselect as needed
         if opts.verbose:
@@ -222,22 +335,15 @@ for gps in args:
     # combine all trgdicts
     #=============================================
     trgdict = event.trigdict()
-    channels = []
     
     ### add kw triggers
     trgdict.add( kwtrgdict )
-    channels += kwchannels
 
     ### add Omicron triggers
     trgdict.add( otrgdict )
-    channels += ochannels
 
     ### add OfflineOmicron triggers
     trgdict.add( ootrgdict )
-    channels += oochannels
-
-    ### clean up channel list to get a unique set
-    channels = list(set(channels))
 
     #=============================================
     # cluster triggers?
@@ -251,64 +357,10 @@ for gps in args:
     if opts.verbose:
         print "\tcomputing statistics, generating plots"
 
-    for chan in channels:
-        n = len(trgdict[chan]) ### number of triggers
-        r = 0.5*n/opts.window ### point estimate of the rate
+    for chan in kwchannels:
+        grand_tour(gps, trgdict[chan][:], "kw", windows, conf=conf)
 
-        if n:
-            min_dt = min(np.abs(np.array([trg[event.col_kw['tcent']] for trg in trgdict[chan]]) - gps))
-        else:
-            min_dt = opts.window
-
-        if r > 0:
-            pvalue = 1 - np.exp(-r*2*min_dt) ### cumulative probability of observing min_dt <= observed min_dt | estimated rate, the factor of 2 comes from looking on either side of the specified gps time
-        else:
-            pvalue = 1 ### limit is not great here...need to add CI
-
-        print "\n\tchannel=%s\n\t-> Ntrg=%d\n\t-> rate=%.9e Hz\n\t-> min_dt=%.9e sec\n\t-> pvalue=%.9e"%(chan, n, r, min_dt, pvalue)
-
-        r_l, r_h = np.array( gci.poisson_bs(conf, n) ) * 0.5 / opts.window
-        pvalue_l = 1 - np.exp(-r_l*2*min_dt)
-        pvalue_h = 1 - np.exp(-r_h*2*min_dt)
-
-        print "\t-> %.5e confidence:\n\t\tlow rate =%.9e Hz\n\t\thigh rate=%.9e Hz\n\t\tlow pvalue =%.9e\n\t\thigh pvalue=%.9e"%(conf, r_l, r_h, pvalue_l, pvalue_h)
-
-
-
-
-
-
-
-    ### SHOULD BE ABLE TO TEST THAT THIS STATISTIC DOES WHAT WE THINK IT SHOULD BY CHECKING RANDOM TIMES AND THE DISTRIBUTION DERIVED THEREFROM
-    ### DO SOMETHING
-    '''
-plot histograms
-    TIME-DOMAIN ONLY
-	zero scale so t=0 corresponds to the gps time
-    FREQ-DOMAIN ONLY
-	
-    TIME-FREQ DOMAIN ONLY
-
-    ALL
-	PLOT FOR EACH CHANNEL SEPARATELY, in some sort of stacked way
-	PLOT for each gps separately?
-
-	markers for each trigger separately
-	stacke multiple gps times into a global histogram
-	compute and report basic statistics:
-		aux rate (need to estimate this over a large window... rely on the usre to do this correctly?)
-			set CI on this rate
-		poisson probability of finding a trigger within "x" of the requested time. set "x" to be the closest time?
-			set CI on this statistic using CI on the rate
-
-scatter aux SNR vs DARM SNR
-	--> possibly inform safety criteria that we can eventually add back into the downselection of events when defining a veto
-
-histogram the dt between triggers in each channel -> confirm "poisson-ianity" of the triggers and the validity of our pvalue estimate
-
-    '''
-
-
-
+    for chan in list(set(ochannels+oochannels)):
+        grand_tour(gps, trgdict[chan][:], "Omicron", windows, conf=conf)
 
 

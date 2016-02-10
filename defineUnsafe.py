612be5739c053a3ace194ba6a86330ab19ed12e3
iDQ <idq@ldas-pcdev1.ligo.caltech.edu>
2016-02-10 11:06:58 -0800

    added a few plotting options

--- defineUnsafe.py
+++ defineUnsafe.py
@@ -23,6 +23,14 @@ parser.add_option("-t", "--tag", default="", type="string")
 
 parser.add_option("-n", "--nperbin", default=8, type="int")
 
+parser.add_option("", "--xmin", default=None, type="float")
+
+parser.add_option("", "--snglchan-histograms", default=False, action="store_true")
+
+parser.add_option("", "--present-histogram", default=False, action="store_true")
+
+parser.add_option("", "--expected-unsafe", default=None, type="string", help="only used to determine colors on histograms")
+
 opts, args = parser.parse_args()
 
 if opts.tag:
@@ -31,6 +39,18 @@ if opts.tag:
 nargs = len(args)
 if not nargs:
     raise ValueError("please supply at least one pointy.out file as an input argument")
+
+if opts.plot or opts.snglchan_histograms:
+    import matplotlib
+    matplotlib.use("Agg")
+    import matplotlib.pyplot as plt
+
+if opts.expected_unsafe != None:
+    file_obj = open(opts.expected_unsafe, "r")
+    expected_unsafe = [line.strip() for line in file_obj.readlines() if line.strip()]
+    file_obj.close()
+else:
+    expected_unsafe = []
 
 #=================================================
 ### load in the lists
@@ -57,7 +77,57 @@ for filename in args:
             chans[chan].append( (p, lines[ind:nind+1], filename) )
             ind = ind
         ind += 1
-chans = dict( [ (key, np.prod([p for p, _, _ in chans[key]])) for key in chans.keys() ] )
+Ntrials = len(args) ### number of trials
+
+if opts.snglchan_histograms:
+    if opts.verbose:
+        print "plotting single-channel histograms:"
+    for chan, pvalues in chans.items():
+        pvalues = np.log10([p[0] for p in pvalues])
+
+        fig = plt.figure()
+        ax = fig.gca()
+
+        n = len(pvalues)
+        ax.hist( pvalues )
+
+        ax.plot( [np.exp(np.sum(np.log(pvalues)/Ntrials))]*2, ax.get_ylim(), 'k-', linewidth=2, alpha=0.5 )
+
+        ax.set_xlabel('log10(pvalue)')
+        ax.set_ylabel('count')
+
+        ax.set_title(chan.replace("_","\_"))
+
+        fig.text( 0.15, 0.95, 'N = %d'%(n), ha='left', va='top' )
+
+        figname = "%s/%04d%s%s.png"%(opts.output_dir, n, chan, opts.tag)
+        if opts.verbose:
+            print "    %s\tN=%d"%(figname, n)
+        fig.savefig( figname )
+        plt.close( fig )
+
+if opts.present_histogram:
+    if opts.verbose:
+        print "plotting histogram of number of appearences"
+
+    fig = plt.figure()
+    ax = fig.gca()
+
+    ax.hist( [len(pvalues) for pvalues in chans.values()], histtype='step' )
+
+    ax.set_xlabel('No. of appearences')
+    ax.set_ylabel('count')
+
+    ax.set_yscale('log')
+    ax.set_ylim(ymin=0.8)
+
+    figname = "%s/appearences%s.png"%(opts.output_dir, opts.tag)
+    if opts.verbose:
+        print "    %s"%(figname)
+    fig.savefig( figname )
+    plt.close( fig )
+
+chans = dict( [ (key, np.exp(np.sum([np.log(p) for p, _, _ in chans[key]])/Ntrials) ) for key in chans.keys() ] )
 
 #=================================================
 ### write the summary file
@@ -71,44 +141,56 @@ file_OBJ = open(fileNAME, "w")
 ### order the lists
 if opts.plot:
     pvalues = []
-keys = sorted(chans.keys())
-for key in keys:
-    pvalue = chans[key]
+    unsafes = []
+items = chans.items()
+items.sort(key=lambda l:l[1])
+for key,pvalue in items:
 
     print >> file_obj, "%.6e\t%s"%(pvalue, key)
     if pvalue <= opts.pvalue:
         print >> file_OBJ, key
 
-    if opts.plot:    
-        pvalues.append( pvalue )
+    if opts.plot:
+        if key in expected_unsafe:
+            unsafes.append( pvalue )
+        else:
+            pvalues.append( pvalue )
 
 #=================================================
 ### make the plot
 if opts.plot:
     if opts.verbose:
         print "plotting"
+    plt.rcParams['text.usetex'] = True
 
-    import matplotlib
-    matplotlib.use("Agg")
-    import matplotlib.pyplot as plt
+    pvalues = [ pvalue for pvalue in pvalues if pvalue > 0]
 
     fig = plt.figure()
     ax = plt.subplot(1,1,1)
 
     nbins = len(pvalues)/opts.nperbin ### could produce poor binning...
-    this_min = min( pvalues )
+    if opts.xmin!=None:
+        this_min = opts.xmin
+    else:
+        this_min = min( pvalues )
 
     bins = np.logspace( np.log10(this_min), 0, nbins)
-    ax.hist( pvalues, bins=bins, histtype="bar", color='g', log=True )
+#    ax.hist( pvalues, bins=bins, histtype="bar", color='g', log=True )
+    ax.hist( [pvalues, unsafes], bins=bins, histtype="barstacked", color=['g', 'r'], log=True )
 
-    ax.set_xlabel('prod{pvalue}')
-    ax.set_ylabel('count')
+    ax.set_xlabel('$\Pi_i\mathrm{pvalue}_i^{1/%d}$'%(Ntrials))
+    ax.set_ylabel('$\mathrm{count}$')
 
     ax.set_xscale("log")
 
     ax.grid(True, which="both")
 
     ax.plot( [opts.pvalue]*2, plt.gca().get_ylim(), 'k--', linewidth=2 )
+
+    if opts.xmin!=None:
+        ax.set_xlim(xmin=opts.xmin)
+    ax.set_xlim(xmax=1.0)
+    ax.set_ylim(ymin=0.1)
 
     figname = "%s/stacked-pvalue%s.png"%(opts.output_dir, opts.tag)
     if opts.verbose:
